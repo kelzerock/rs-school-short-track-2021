52bcf16285d3a264add83b3ccdc57bd2a63f8bfd
kelzerock <kelzzzer@gmail.com>
2021-04-11 00:01:59 +0300

    add: complite task 02, 06,07

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -12,8 +12,24 @@
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  let arr = n.toString().split('-');
+  if (arr.length !== 6) {
+    return false;
+  }
+  arr = arr.join('').split('');
+  for (let i = 0; i < arr.length; i++) {
+    let integer = 0;
+    let liter = 0;
+    if (arr[i] >= 0 && arr[i] <= 9) {
+      integer = 1;
+    }
+    if (arr[i] >= 'A' && arr[i] <= 'F') {
+      liter = 1;
+    }
+    if (integer === 0 && liter === 0) return false;
+  }
+  return true;
 }
 
 module.exports = isMAC48Address;
--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,32 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const result = [];
+  for (let i = 0; i < names.length; i++) {
+    let count = 0;
+    for (let j = 0; j < result.length; j++) {
+      if (result[j] === names[i]) {
+        for (let x = 0; x < result.length; x++) {
+          if (result[x] === names[i]) { count++; }
+          const regex = /(\d*)$/;
+          if (regex.test(result[x])) {
+            if (result[x].slice(0, result[x].length - 3) === names[i]) {
+              count++;
+            }
+          }
+        }
+      }
+    }
+    let summary;
+    if (count === 0) {
+      summary = names[i];
+    } else {
+      summary = `${names[i]}(${count})`;
+    }
+    result.push(summary);
+  }
+  return result;
 }
 
 module.exports = renameFiles;
