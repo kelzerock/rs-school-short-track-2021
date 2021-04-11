7a8d8acf4e33e4d7c2f879112cde52ec2eb47ab1
kelzerock <kelzzzer@gmail.com>
2021-04-11 17:02:14 +0300

    add: complete task 11 and 15

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,31 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const result = [];
+  let i = 0;
+  while (i < str.length) {
+    const lastLetter = result[result.length - 1];
+    let clearLetter;
+    if (lastLetter !== undefined) {
+      clearLetter = lastLetter.slice(lastLetter.length - 1, lastLetter.length);
+    } else { clearLetter = 0; }
+    if (str[i] !== clearLetter) {
+      if (str[i] !== str[i + 1]) {
+        result.push(str[i]);
+      } else {
+        let count = 1;
+        let x = 0;
+        while (str[i] === str[i + 1 + x]) {
+          x++;
+        }
+        count += x;
+        result.push(count + str[i]);
+      }
+    }
+    i++;
+  }
+  return result.join('');
 }
 
 module.exports = encodeLine;
--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,8 +8,10 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  const placeA = email.lastIndexOf('@');
+  const result = email.slice(placeA + 1, email.length);
+  return result;
 }
 
 module.exports = getEmailDomain;
