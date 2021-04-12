dc2ab8788738eb561bb15777b15f1719d1233f04
kelzerock <kelzzzer@gmail.com>
2021-04-12 21:59:20 +0300

    add: task 14 complite

--- src/14-queue.js
+++ src/14-queue.js
@@ -1,4 +1,4 @@
-// const ListNode = require('../extensions/list-node');
+const ListNode = require('../extensions/list-node');
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
  *
@@ -12,16 +12,44 @@
  */
 
 class Queue {
-  get size() {
-    throw new Error('Not implemented');
+  constructor() {
+    this.head = null;
+    this.tail = null;
   }
 
-  enqueue(/* element */) {
-    throw new Error('Not implemented');
+  getsize() {
+    const nodes = [];
+    let currentNode = this.head;
+    while (currentNode) {
+      nodes.push(currentNode);
+      currentNode = currentNode.next;
+    }
+    return nodes.length;
+  }
+
+  enqueue(element) {
+    const newNode = new ListNode(element);
+    if (!this.head || !this.tail) {
+      this.head = newNode;
+      this.tail = newNode;
+      return this;
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
+    return this;
   }
 
   dequeue() {
-    throw new Error('Not implemented');
+    if (!this.head) {
+      return undefined;
+    }
+    const deletedHead = this.head.value;
+    if (this.head.next) {
+      this.head = this.head.next;
+    } else {
+      this.head = null;
+    }
+    return deletedHead;
   }
 }
 
